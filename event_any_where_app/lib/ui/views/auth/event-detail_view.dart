4fee21186d41b175ee7105a62ca3d1d18cae19a9
datonefeed <alexftn1182003@gmail.com>
2024-10-15 00:13:09 +0700

    Create: EventDetailScreen v1

--- event_any_where_app/lib/ui/views/auth/event-detail_view.dart
+++ event_any_where_app/lib/ui/views/auth/event-detail_view.dart
@@ -1,1 +1,15 @@
-//Test commit
+import 'package:flutter/material.dart';
+
+class EventDetailScreen extends StatefulWidget {
+  const EventDetailScreen({super.key});
+
+  @override
+  State<EventDetailScreen> createState() => _EventDetailScreenState();
+}
+
+class _EventDetailScreenState extends State<EventDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
